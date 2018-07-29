d5b7662fc47d58e9ec39d8fd6af383383042e8d7
Hp <mahshidga@gmail.com>
2018-07-29 15:08:20 +0430

    initial commit

--- app/src/main/java/com/example/yara/CategoryFragment.java
+++ app/src/main/java/com/example/yara/CategoryFragment.java
@@ -7,20 +7,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class RegisterFragment extends Fragment {
+
+
+public class CategoryFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public RegisterFragment() {
+    public CategoryFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        return inflater.inflate(R.layout.fragment_category, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
